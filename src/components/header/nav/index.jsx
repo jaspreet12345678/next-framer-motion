fbb3f4e727855a67a0ad1643b12fe90249a9f76b
Lakhan <lakhan.mango@gmail.com>
2024-08-24 13:52:12 +0530

    updated

--- src/components/header/nav/index.jsx
+++ src/components/header/nav/index.jsx
@@ -4,45 +4,56 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { height } from '../anim';
 import Body from './Body';
-import Footer from './Footer';
 import Image from './Image';
 
 const links = [
   {
     title: "Home",
     href: "/",
-    src: "home.png"
+    src: "home.png",
+    alt: "Home"
   },
   {
     title: "Shop",
     href: "/shop",
-    src: "shop.png"
+    src: "shop.png",
+    alt: "Shop"
   },
   {
     title: "About Us",
     href: "/about",
-    src: "home.png"
+    src: "about.png", 
+    alt: "About Us"
   },
   {
     title: "Contact",
     href: "/contact",
-    src: "contact.png"
+    src: "contact.png",
+    alt: "Contact"
   }
-]
+];
 
 export default function Index() {
-
-  const [selectedLink, setSelectedLink] = useState({isActive: false, index: 0});
+  const [selectedLink, setSelectedLink] = useState({ isActive: false, index: 0 });
 
   return (
-    <motion.div variants={height} initial="initial" animate="enter" exit="exit" className={styles.nav}>
+    <motion.div
+      variants={height}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className={styles.nav}
+    >
       <div className={styles.wrapper}>
         <div className={styles.container}>
-          <Body links={links} selectedLink={selectedLink} setSelectedLink={setSelectedLink}/>
-          {/* <Footer /> */}
+          <Body links={links} selectedLink={selectedLink} setSelectedLink={setSelectedLink} />
         </div>
-        <Image src={links[selectedLink.index].src} isActive={selectedLink.isActive}/>
+        <Image
+          src={links[selectedLink.index].src}
+          alt={links[selectedLink.index].alt}
+          isActive={selectedLink.isActive}
+        />
       </div>
     </motion.div>
-  )
+  );
 }
